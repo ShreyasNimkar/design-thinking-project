1fd8fe8e9bc2a9dced4058aa5549272c7089f0ea
ShreyasNimkar <nimkarshreyas22@gmail.com>
2023-03-27 18:47:59 +0530

    feat:shreyas-first-commit

---
+++ src/data/login/login.js
@@ -0,0 +1,20 @@
+const usernames = {
+  1: {
+    username: "shreyas",
+    password: "password",
+  },
+  2: {
+    username: "shreyas",
+    password: "password",
+  },
+  3: {
+    username: "shreyas",
+    password: "password",
+  },
+  4: {
+    username: "shreyas",
+    password: "password",
+  },
+};
+
+export default usernames;
